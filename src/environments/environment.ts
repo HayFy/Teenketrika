4daa37d250fb09ade22ae2e3038910fb0b71f4d9
ranja <mirijaraoulison@gmail.com>
2022-08-02 16:10:08 +0300

    Premier commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'teenketrika-dev',
+    appId: '1:4994355812:web:bda09c0ee6883ca1ddc571',
+    databaseURL: 'https://teenketrika-dev-default-rtdb.firebaseio.com',
+    storageBucket: 'teenketrika-dev.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDzIG8cirokrPuzriWy1vRvP09ILa8MaLo',
+    authDomain: 'teenketrika-dev.firebaseapp.com',
+    messagingSenderId: '4994355812',
+  },
   production: false
 };
 
